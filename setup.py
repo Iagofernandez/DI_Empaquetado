483a4aaad7d06a7fd377d46c0d4fb8109c4fc819
Iago <ifernandezblanco@danielcastelao.org>
2020-02-20 10:14:34 +0100

    Fourth commit Algunas explicaciones de comandos puestas en setup.py Poner en la ventana terminal de nuestro programa

--- setup.py
+++ setup.py
@@ -1,31 +1,38 @@
 from setuptools import setup
 
+# coding=utf-8
 descripcion_longa = open('Readme.txt').read()
 setup(
     name="Exemplo de empaquetado",
-    version="0.12",
+    version="0.11",
     author="Iago",
     author_email="ifernandezblanco@danielcastelao.org",
     url="https://www.danielcastelao.org",
     license="GLP",
     platforms="Unix",
     classifiers=["Development Status :: 3 - Alpha",
-                "Environment :: Console",
-                "Topic :: Software Development :: Libraries",
-                "License :: OSI Aproved :: GNU General Public License",
-                "Programming Language :: Python :: 3.4",
-                "Operating System :: Linux Ubuntu"
-                ],
+                 "Environment :: Console",
+                 "Topic :: Software Development :: Libraries",
+                 "License :: OSI Aproved :: GNU General Public License",
+                 "Programming Language :: Python :: 3.4",
+                 "Operating System :: Linux Ubuntu"
+                 ],
     description="Descripción breve del Ejemplo de empaquetado",
     long_description=descripcion_longa,
     keywords="empaquetado instalador paquetes",
     packages=['paquete', 'paquete/subpaquete'],
     # OTRA FORMA: packages = find_packages(exclude= ['*.test','*.test.*']) podemo excluír lo que queramos
-    #package_data={
-        #'paquete1': 'notas.txt',
-       # 'paquete/subPaquete1': 'texto.txt'
-   # },
-   # data_files=[('datos', 'dat/datos.txt')],
+    package_data={
+        'paquete' : 'notas.txt',
+        'paquete/subpaquete': 'texto.txt'
+    },
+    # data_files=[('datos', 'dat/datos.txt')],
     entry_points={'console_scripts': ['imprimeAlgo = paquete1.moduloPaquete1: main', ], }
 
 )
+
+# Necesario
+# pip install --upgrade python3-dev
+# pip install --upgrade python3
+# python3 setup.py sdist
+#  python setup.py install
